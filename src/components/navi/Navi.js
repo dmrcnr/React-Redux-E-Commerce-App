c3a5e987445309b36a4324823c866ce70bae584b
Mustafa Caner Demir <dmrcnr@gmail.com>
2022-04-25 23:28:15 +0300

    Unused import deleted from Navi.js

--- src/components/navi/Navi.js
+++ src/components/navi/Navi.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Navbar, NavbarBrand, Nav, NavbarToggler, NavItem } from "reactstrap";
 import CartSummary from "../cart/CartSummary";
 
